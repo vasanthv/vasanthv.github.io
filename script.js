386aaf2b5a7ffac7f51b8288c00bfacca8fc56ab
Vasanth.V <imvasanthv@gmail.com>
2020-02-29 21:14:01 +0530

    Pull down to touch

--- script.js
+++ script.js
@@ -1,42 +1,20 @@
-let pulled = 0;
-let distancePulled = 0;
-let switchedOn = false;
-let switchingOn = false;
+let lightOn = false;
 const pointerDown = e => {
-	pulled = e.clientY;
-	switchingOn = !switchedOn;
-	lampSwitch.style.transition = 'none';
-};
-const pointerMove = e => {
-	if (pulled) {
-		distancePulled = e.clientY - pulled;
-		if (distancePulled > 0) {
-			if (distancePulled < 20) {
-				lampSwitch.style.transform = `translateY(${distancePulled - 30}px)`;
-			} else {
-				lampSwitch.style.transform = `translateY(-10px)`;
-				toggleLight();
-			}
-		}
-	}
+	lampSwitch.style.transform = `translateY(-15px)`;
+	toggleLight();
 };
 const pointerEnd = e => {
-	switchingOn = !switchedOn;
-	pulled = 0;
-	distancePulled = 0;
-	lampSwitch.style.transition = 'transform 500ms';
 	lampSwitch.style.transform = `translateY(-30px)`;
 };
 const toggleLight = () => {
-	if (!switchedOn && switchingOn) {
-		switchedOn = true;
+	if (!lightOn) {
+		lightOn = true;
 		lampClick.play();
 		light.classList.add('on');
 		content.style.visibility = 'visible';
 		lampCircle.style.fill = '#dfdfdf';
-	}
-	if (switchedOn && !switchingOn) {
-		switchedOn = false;
+	} else {
+		lightOn = false;
 		lampClick.play();
 		light.classList.remove('on');
 		content.style.visibility = 'hidden';
@@ -49,6 +27,4 @@ const content = document.getElementById('content');
 const lampClick = document.getElementById('click');
 const lampCircle = document.getElementById('lampCircle');
 lampSwitch.addEventListener('mousedown', pointerDown);
-lampSwitch.addEventListener('mousemove', pointerMove);
 document.addEventListener('mouseup', pointerEnd);
-// lampSwitch.addEventListener('mouseout', pointerEnd);
